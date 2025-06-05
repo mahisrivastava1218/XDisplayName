75429a99d1126be254f992016dd6913e1f6ad86a
Mahima <priyanshisrivastava708016@gmail.com>
2025-06-05 12:24:13 +0530

    nameDisplay

--- buildout/src/Display/Display.jsx
+++ buildout/src/Display/Display.jsx
@@ -21,17 +21,17 @@ export default function Display(){
         <form onSubmit={handleSubmit} style={{border:"1px solid green",height:"100vh",width:"100vw",display:"flex",flexDirection:"column",alignItems:"center",gap:"20px"}}>
             <h1>Full Name Display</h1>
             <div>
-                First Name: {" "}
+                First Name:
                 <input type="text" value={firstName} placeholder="enter your fullName" className={styles["placeholder"]} onChange={(e)=>{
-                    const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
-                    setFirstName(onlyLetters)
+                    // const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
+                    setFirstName(e.target.value)
                     }} required/>
             </div>
             <div>
-                Last Name: {" "}
+                Last Name:
                 <input type="text" value={lastName} placeholder="enter your LastName" className={styles["placeholder"]} onChange={(e)=>{
-                      const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
-                      setLastName(onlyLetters)
+                    //   const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
+                      setLastName(e.target.value)
                 }} required/>
             </div>
             <button type="submit" style={{padding:"10px"}}>Submit</button>
