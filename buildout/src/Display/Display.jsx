e2ccea09f0e6a626446a5b010e54da508ad9ce9a
Mahima <priyanshisrivastava708016@gmail.com>
2025-06-05 13:00:35 +0530

    remove space

--- buildout/src/Display/Display.jsx
+++ buildout/src/Display/Display.jsx
@@ -1,17 +1,14 @@
 import {useState } from "react";
-import { useNavigate } from "react-router-dom";
 import styles from "./Display.module.css";
 export default function Display(){
         const [firstName,setFirstName]=useState("");
         const[lastName,setLastName]=useState("");
         const[display,setDisplay] = useState(false);
-        const navigate = useNavigate();
     console.log(firstName,lastName);
     const handleSubmit=(e)=>{
         e.preventDefault();
          if(firstName.trim() && lastName.trim()){
                 setDisplay(true);
-                navigate("/");
             }else{
                 setDisplay(false);
         }
@@ -23,20 +20,20 @@ export default function Display(){
             <div>
                 First Name:
                 <input type="text" value={firstName} placeholder="enter your fullName" className={styles["placeholder"]} onChange={(e)=>{
-                    const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
-                    setFirstName(onlyLetters)
+                    // const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
+                    setFirstName(e.target.value)
                     }} required/>
             </div>
             <div>
                 Last Name:
                 <input type="text" value={lastName} placeholder="enter your LastName" className={styles["placeholder"]} onChange={(e)=>{
-                      const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
-                      setLastName(onlyLetters)
+                    //   const onlyLetters= e.target.value.replace(/[^a-zA-z]/g,'');
+                      setLastName(e.target.value)
                 }} required/>
             </div>
             <button type="submit" style={{padding:"10px"}}>Submit</button>
             {display && (
-                 <div style={{color:"black"}}data-testid="full-name-display">
+                 <div style={{color:"black"}}>
                     Full Name: {firstName} {lastName}
                  </div>
             )}
